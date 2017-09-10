7c19245e221b043ad480f828e315f29768731fc0
Justin Tooley <jstooley823@gmail.com>
2017-09-10 00:30:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :color, :size, :material
+  attr_reader :brand
 
   def initialize(title)
     @title = title
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def coble
+    puts "THe show condition has been repaired!"
   end
 
 end
