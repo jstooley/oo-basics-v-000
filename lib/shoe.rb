5c42b131ee9c4a3b90a9a17702ca168f820499ce
Justin Tooley <jstooley823@gmail.com>
2017-09-10 00:28:23 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+Shoe.new("And Then There Were None")
